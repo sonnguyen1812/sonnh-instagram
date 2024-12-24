96123e81c4bba5abb199f95050e5ca66662399e8
Son Nguyen <son18122001@gmail.com>
2024-12-24 11:14:51 +0700

    Update alias.js

--- alias.js
+++ alias.js
@@ -1,6 +1,7 @@
 System.config({
   paths: {
     '@assets/*': './packages/assets/src/*',
-    '@functions/*': './packages/functions/src/*'
+    '@functions/*': './packages/functions/src/*',
+    '@scripttag/*': './packages/scripttag/src/*',
   }
 });

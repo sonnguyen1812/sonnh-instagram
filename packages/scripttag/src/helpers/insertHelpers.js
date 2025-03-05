e20c819af1bcf785faadbfa0ed7ddc1dfa068222
Son Nguyen <son18122001@gmail.com>
2025-03-05 10:32:03 +0700

    add scripttag (removed recently)

---
+++ packages/scripttag/src/helpers/insertHelpers.js
@@ -0,0 +1,12 @@
+// packages/scripttag/src/helpers/insertHelpers.js
+export const insertAfter = (el, referenceNode) => {
+  referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+};
+
+export const insertBefore = (el, referenceNode) => {
+  referenceNode.parentNode.insertBefore(el, referenceNode);
+};
+
+export const insertInside = (el, referenceNode) => {
+  referenceNode.parentNode.appendChild(el);
+};
